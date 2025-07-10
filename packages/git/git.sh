9517452969150e7f42059853c7cd2344d135b7c3
benja2998 <benja2998@duck.com>
2025-07-10 11:32:57 -0400

    Update git.sh

--- packages/git/git.sh
+++ packages/git/git.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
 
-# Check if git is installed
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing via winget..."
-    winget install --id Git.Git -e --source winget
-fi
-
+git --version &>/dev/null || winget install --id Git.Git -e --source winget
 git "$@"
