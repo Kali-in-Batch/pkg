91f8cb2c73f963307b4c70e6ff1c52567fe4b2ad
benja2998 <197428407+benja2998@users.noreply.github.com>
2025-07-12 19:30:12 -0400

    Update preinstall.sh

--- packages/git/preinstall.sh
+++ packages/git/preinstall.sh
@@ -2,9 +2,8 @@
 
 winget install --id Git.Git -e --source winget
 
-echo "Adding alias to current session"
-alias git='source /usr/bin/git' # For the current session
 if ! grep -q "alias git='source /usr/bin/git'" ~/.bashrc; then
     echo "Adding alias to .bashrc"
     echo "alias git='source /usr/bin/git'" >> ~/.bashrc # For all future sessions
+    echo "Please run 'source ~/.bashrc' before running git, otherwise it will not work properly!"
 fi
