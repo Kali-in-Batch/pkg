855e0d52b80b7f3c67a77f35083f4143b12fb716
benja2998 <197428407+benja2998@users.noreply.github.com>
2025-07-12 18:26:11 -0400

    Update git.sh

--- packages/git/git.sh
+++ packages/git/git.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
-IFS=; read -ra dirs <<< "$PATH_OLD"
-for dir in "${dirs[@]}"; do
+# Extract PATH_OLD value from `export` output
+path_old=$(export | grep '^declare -x PATH_OLD=' | sed -E 's/^declare -x PATH_OLD="(.*)"$/\1/')
+
+# Replace colons and semicolons with newlines, then check each path
+echo "$path_old" | tr ':;' '\n' | while read -r dir; do
   if [[ -x "$dir/git.exe" ]]; then
     echo "$dir/git.exe"
     break
