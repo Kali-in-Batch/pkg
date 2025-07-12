268afde0ac28a67df25831375ca402a2851386bb
benja2998 <197428407+benja2998@users.noreply.github.com>
2025-07-12 19:04:17 -0400

    Update preinstall.sh

--- packages/git/preinstall.sh
+++ packages/git/preinstall.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 winget install --id Git.Git -e --source winget
+echo "Adding \"\"alias git='source /usr/bin/git'\"\" to .bashrc if not already there"
+
+if ! grep -q "alias git='source /usr/bin/git'" ~/.bashrc; then
+    echo "alias git='source /usr/bin/git'" >> ~/.bashrc
+fi
